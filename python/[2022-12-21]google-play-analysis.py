84d5cc7f5b1a4ba40d2a96c36fad2fd46d102079
RoyYangsCodingDiary <117495906+RoyYangsCodingDiary@users.noreply.github.com>
2022-12-21 19:43:21 -0500

    Create [2022-12-21]google-play-analysis.py

---
+++ python/[2022-12-21]google-play-analysis.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+# Guided project by Datacamp
+# Published: December 11, 2022
+
+import pandas as pd
+import plotly
+plotly.offline.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import seaborn as sns
+import warnings
+sns.set_style("darkgrid")
+warnings.filterwarnings("ignore")
+
+apps_with_duplicates = pd.read_csv('datasets/apps.csv')
+
+apps = apps_with_duplicates.drop_duplicates()
+
+# Print the total number of apps
+print('Total number of apps in the dataset = ', apps.size)
+
+print(apps.head())
+
+# List of characters to remove
+chars_to_remove = ["+",",","$"]
+# List of column names to clean
+cols_to_clean = ["Installs", "Price"]
+
+for col in cols_to_clean:
+    for char in chars_to_remove:
+        # Replace the character with an empty string
+        apps[col] = apps[col].apply(lambda x: x.replace(char, ""))
+        
+print(apps.info())
+
+import numpy as np
+
+apps["Installs"] = apps["Installs"].astype(np.float64)
+
+apps["Price"] = apps["Price"].astype(np.float64)
+
+print(apps.dtypes)
+
+num_categories = apps["Category"].nunique()
+print('Number of categories = ', num_categories)
+
+num_apps_in_category = apps["Category"].value_counts()
+
+sorted_num_apps_in_category = num_apps_in_category.sort_values(ascending=False)
+
+data = [go.Bar(
+        x = num_apps_in_category.index, # index = category name
+        y = num_apps_in_category.values, # value = count
+)]
+
+plotly.offline.iplot(data)
+
+avg_app_rating = apps['Rating'].mean()
+print('Average app rating = ', avg_app_rating)
+
+data = [go.Histogram(
+        x = apps['Rating']
+)]
+
+# Vertical dashed line to indicate the average app rating
+layout = {'shapes': [{
+              'type' :'line',
+              'x0': avg_app_rating,
+              'y0': 0,
+              'x1': avg_app_rating,
+              'y1': 1000,
+              'line': { 'dash': 'dashdot'}
+          }]
+          }
+
+plotly.offline.iplot({'data': data, 'layout': layout})
+
+# Select rows where both 'Rating' and 'Size' values are present (ie. the two values are not null)
+apps_with_size_and_rating_present = apps.dropna(subset=['Rating','Size'])
+
+# Subset for categories with at least 250 apps
+large_categories = apps_with_size_and_rating_present.groupby('Category').filter(lambda x: len(x) >= 250)
+
+plt1 = sns.jointplot(x = 'Size', y = 'Rating', data=large_categories)
+
+paid_apps = apps_with_size_and_rating_present[apps_with_size_and_rating_present['Type'] == 'Paid']
+
+plt2 = sns.jointplot(x = 'Price', y = 'Rating', data=paid_apps)
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 8)
+
+popular_app_cats = apps[apps.Category.isin(['GAME', 'FAMILY', 'PHOTOGRAPHY',
+                                            'MEDICAL', 'TOOLS', 'FINANCE',
+                                            'LIFESTYLE','BUSINESS'])]
+
+# Examine the price trend by plotting Price vs Category
+ax = sns.stripplot(x = popular_app_cats['Price'], y = popular_app_cats['Category'], jitter=True, linewidth=1)
+ax.set_title('App pricing trend across categories')
+
+apps_above_200 = apps[apps['Price'] > float(200)][['Category', 'App', 'Price']]
+
+apps_under_100 = popular_app_cats[popular_app_cats['Price'] < 100]
+
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 8)
+
+# Examine price vs category with the authentic apps (apps_under_100)
+ax = sns.stripplot(x = 'Price', y = 'Category', data = apps_under_100, jitter = True, linewidth = 1)
+ax.set_title('App pricing trend across categories after filtering for junk apps')
+
+trace0 = go.Box(
+    # Data for paid apps
+    y = apps[apps['Type'] == 'Paid']['Installs'],
+    name = 'Paid'
+)
+
+trace1 = go.Box(
+    # Data for free apps
+    y = apps[apps['Type'] == 'Free']['Installs'],
+    name = 'Free'
+)
+
+layout = go.Layout(
+    title = "Number of downloads of paid apps vs. free apps",
+    yaxis = dict(title = "Log number of downloads",
+                type = 'log',
+                autorange = True)
+)
+
+# Add trace0 and trace1 to a list for plotting
+data = [trace0, trace1]
+plotly.offline.iplot({'data': data, 'layout': layout})
+
+reviews_df = pd.read_csv('datasets/user_reviews.csv')
+
+# Join the two dataframes
+merged_df = apps.merge(reviews_df, on='App', how='inner')
+
+# Drop NA values from Sentiment and Review columns
+merged_df = merged_df.dropna(subset = ['Sentiment', 'Review'])
+
+sns.set_style('ticks')
+fig, ax = plt.subplots()
+fig.set_size_inches(11, 8)
+
+# User review sentiment polarity for paid vs. free apps
+ax = sns.boxplot(x = 'Type', y = 'Sentiment_Polarity', data = merged_df)
+ax.set_title('Sentiment Polarity Distribution')
