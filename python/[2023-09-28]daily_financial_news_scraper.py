c2339ee37379328d092798dd341c3ac4b67694a8
RoyYangsCodingDiary <117495906+RoyYangsCodingDiary@users.noreply.github.com>
2023-08-30 12:11:36 -0400

    Update [2023-09-28]daily_financial_news_scraper.py

--- python/[2023-09-28]daily_financial_news_scraper.py
+++ python/[2023-09-28]daily_financial_news_scraper.py
@@ -1,91 +1,155 @@
-### Sample Output:
+### Sample Output
 
-## 8/28/2023
 #
-#SPX: 4426.03
-#DJI: 34441.64
-#NASDAQ: 13695.4
-#S&P/TSX: 19900.31
-#VIX: 16.24
-#USD/CAD: 1.3597
-#EUR/USD: 1.0824
-#USD/CNY: 7.2895
+## 8/30/2023
+#
+#SPX: 4500.34
+#DJI: 34847.8
+#NASDAQ: 13961.8
+#S&P/TSX: 20319.44
+#Russell 2000: 1893.47
+#Nikkei 225: 32432.95
+#FTSE 100: 7464.99
+#Hang Seng Index: 18659.96
+#VIX: 14.53
+#US Dollar Index: 103.53
+#USD/CAD: 1.3552
+#EUR/USD: 1.0884
+#USD/CNY: 7.2791
 # 
-#US 1 Month Yield: 5.40%
-#US 3 Month Yield: 5.46%
-#US 6 Month Yield: 5.59%
-#US 1 Year Yield: 5.46%
-#US 2 Year Yield: 5.00%
-#US 3 Year Yield: 4.69%
-#US 5 Year Yield: 4.37%
-#US 7 Year Yield: 4.32%
-#US 10 Year Yield: 4.19%
-#US 30 Year Yield: 4.28%
+#US 1 Month Yield: 5.39%
+#US 3 Month Yield: 5.47%
+#US 6 Month Yield: 5.54%
+#US 1 Year Yield: 5.39%
+#US 2 Year Yield: 4.85%
+#US 3 Year Yield: 4.54%
+#US 5 Year Yield: 4.25%
+#US 7 Year Yield: 4.20%
+#US 10 Year Yield: 4.10%
+#US 30 Year Yield: 4.23%
 # 
-#Gold Spot:	$1,925.17, 0.26% from last close $1,920.23
-#Palladium:	$1,247.50, -0.87% from last close $1,258.50
-#Platinum Spot:	$972.00, 0.26% from last close $969.50
-#Silver Spot:	$24.29, 0.04% from last close $24.28
-#Natural Gas:	$2.55, -1.16% from last close $2.58
-#Heating Oil:	$84.54, 0.0% from last close $84.54
-#Oil (Brent):	$84.39, -0.04% from last close $84.42
-#Oil (WTI):	$80.05, 0.1% from last close $80.10
-#Aluminium:	$2,155.85, -0.07% from last close $2,155.85
-#Lead Spot:	$2,177.00, -0.81% from last close $2,177.00
-#Nickel Spot:	$20,886.50, 2.41% from last close $20,886.50
-#Zinc Spot:	$2,375.25, 0.52% from last close $2,375.25
-#Cotton Spot:	$0.86, -0.8% from last close $0.87
-#Oats Spot:	$4.81, 0.57% from last close $4.83
-#Coffee Spot:	$1.53, -0.97% from last close $1.52
-#Cocoa Spot:	$2,785.00, 1.98% from last close $2,782.00
-#Lean Hog Spot:	$0.82, 2.54% from last close $0.82
-#Corn Spot:	$4.76, -0.68% from last close $4.79
-#Feeder Cattle:	$2.50, 1.18% from last close $2.50
-#Milk Spot:	$17.21, 0.06% from last close $17.20
-#Orange Juice:	$3.19, -1.65% from last close $3.22
-#Soybeans Spot:	$13.91, -0.3% from last close $13.95
-#Wheat Spot:	$228.25, -1.93% from last close $228.75
-#Sugar Spot:	$0.26, 2.93% from last close $0.26
+#Gold Spot:	$1,947.24, 0.5% from last close $1,937.48
+#Palladium:	$1,230.50, -1.72% from last close $1,252.00
+#Platinum Spot:	$976.50, -0.46% from last close $981.00
+#Silver Spot:	$24.80, 0.2% from last close $24.75
+#Natural Gas:	$2.72, 8.14% from last close $2.56
+#Heating Oil:	$83.48, -1.56% from last close $84.80
+#Oil (Brent):	$86.09, 0.65% from last close $85.49
+#Oil (WTI):	$81.88, 0.73% from last close $81.16
+#Aluminium:	$2,168.35, 0.58% from last close $2,168.35
+#Lead Spot:	$2,174.00, -0.14% from last close $2,174.00
+#Nickel Spot:	$20,351.50, -2.56% from last close $20,351.50
+#Zinc Spot:	$2,419.00, 1.84% from last close $2,419.00
+#Cotton Spot:	$0.87, 0.41% from last close $0.87
+#Oats Spot:	$4.94, 0.87% from last close $4.91
+#Coffee Spot:	$1.52, 1.7% from last close $1.51
+#Cocoa Spot:	$2,865.00, 2.87% from last close $2,870.00
+#Lean Hog Spot:	$0.81, -0.03% from last close $0.81
+#Corn Spot:	$4.69, -0.21% from last close $4.70
+#Feeder Cattle:	$2.50, -0.29% from last close $2.51
+#Milk Spot:	$19.12, 11.29% from last close $17.18
+#Orange Juice:	$3.32, 1.26% from last close $3.24
+#Soybeans Spot:	$13.79, -0.2% from last close $13.82
+#Wheat Spot:	$226.00, 0.33% from last close $225.25
+#Sugar Spot:	$0.26, 1.1% from last close $0.25
 # 
 #### Holidays
-#United Kingdom - Bank Holiday
+#No market holidays today
 #
 #### Speeches
-#BCB Focus Market Readout at 07:25
-#German Buba President Nagel Speaks at 08:00
-#German Buba Balz Speaks at 12:00
+#BoJ Board Member Nakamura Speaks at 21:30
 #
 #### Economic Releases Today
-#01:00: JPY Coincident Indicator (MoM) at 0.8%, missing consensus of 0.9% vs. previous 0.1%
-#01:00: JPY Leading Index (MoM) at -0.2%, at consensus of -0.2% vs. previous 1.1%
-#01:00: JPY Leading Index at 108.9, at consensus of 108.9 vs. previous 109.2
-#04:00: EUR M3 Money Supply (YoY) (Jul) at -0.4%, missing consensus of 0.0% vs. previous 0.6%
-#04:00: EUR Loans to Non Financial Corporations (Jul) at 2.2%, missing consensus of 2.5% vs. previous 3.0%
-#04:00: EUR Private Sector Loans (YoY) at 1.3%, missing consensus of 1.4% vs. previous 1.7%
-#07:30: BRL Bank lending (MoM) (Jul) at -0.2%, missing consensus of -0.1% vs. previous 0.1%
-#08:55: EUR French 12-Month BTF Auction at 3.631% vs. previous 3.639%
-#08:55: EUR French 3-Month BTF Auction at 3.664% vs. previous 3.684%
-#08:55: EUR French 6-Month BTF Auction at 3.671% vs. previous 3.658%
-#10:30: USD Dallas Fed Mfg Business Index (Aug) at -17.2, beating consensus of -21.6 vs. previous -20.0
-#11:30: USD 2-Year Note Auction at 5.024% vs. previous 4.823%
-#11:30: USD 6-Month Bill Auction at 5.350% vs. previous 5.295%
-#13:00: USD 3-Month Bill Auction at 5.340% vs. previous 5.300%
-#13:00: USD 5-Year Note Auction at 4.400% vs. previous 4.170%
-#19:01: GBP BRC Shop Price Index (YoY) at 6.9% vs. previous 7.6%
-#19:30: JPY Jobs/applications ratio (Jul) at 1.29, missing consensus of 1.30 vs. previous 1.30
-#19:30: JPY Unemployment Rate (Jul) at 2.7%, beating consensus of 2.5% vs. previous 2.5%
-#23:35: JPY 2-Year JGB Auction releasing soon, previous figure at -0.062% 
+# Time: | Actual | Forecast | Previous | Currency | Event |
+#01:00: 	36.2	37.5	37.1 | JPY Household Confidence (Aug)
+#01:30: 	5.9%	NA	5.8% | EUR North Rhine Westphalia CPI (YoY) (Aug)
+#02:00: 	-14.2%	NA	-7.0% | EUR Italian Car Registration (MoM) (Jul)
+#02:00: 	8.7%	NA	9.1% | EUR Italian Car Registration (YoY) (Jul)
+#02:00: 	-13.2%	NA	13.4% | EUR German Car Registration (MoM) (Jul)
+#02:00: 	18.1%	NA	24.8% | EUR German Car Registration (YoY) (Jul)
+#02:00: 	-0.6%	0.0%	-1.6% | EUR German Import Price Index (MoM) (Jul)
+#02:00: 	-13.2%	-12.9%	-11.4% | EUR German Import Price Index (YoY) (Jul)
+#02:00: 	-32.4%	NA	31.1% | EUR French Car Registration (MoM) (Jul)
+#02:00: 	19.9%	NA	11.5% | EUR French Car Registration (YoY) (Jul)
+#03:00: 	2.6%	2.6%	2.3% | EUR Spanish CPI (YoY) (Aug)
+#03:00: 	0.5%	0.4%	0.2% | EUR Spanish CPI (MoM) (Aug)
+#03:00: 	2.4%	2.5%	2.1% | EUR Spanish HICP (YoY) (Aug)
+#03:00: 	0.5%	0.6%	-0.1% | EUR Spanish HICP (MoM) (Aug)
+#03:00: 	93.3	93.7	94.5 | EUR Business and Consumer Survey (Aug)
+#03:00: 	-16.0	-16.0	-15.1 | EUR Consumer Confidence (Aug)
+#03:00: 	3.6	2.0	3.4 | EUR Selling Price Expectations (Aug)
+#03:00: 	3.9	4.2	5.4 | EUR Services Sentiment (Aug)
+#03:00: 	-10.3	-9.9	-9.3 | EUR Industrial Sentiment (Aug)
+#04:00: 	97.8	98.0	99.1 | EUR Italian Business Confidence (Aug)
+#04:00: 	106.5	105.6	106.7 | EUR Italian Consumer Confidence (Aug)
+#04:00: 	0.3%	NA	0.2% | EUR Baden Wuerttemberg CPI (MoM)
+#04:00: 	7.0%	NA	6.8% | EUR Baden Wuerttemberg CPI (YoY) (Aug)
+#04:00: 	5.9%	NA	6.1% | EUR Bavaria CPI (YoY) (Aug)
+#04:00: 	0.3%	NA	0.4% | EUR Bavaria CPI (MoM) (Aug)
+#04:00: 	6.8%	NA	6.7% | EUR Brandenburg CPI (YoY) (Aug)
+#04:00: 	0.2%	NA	0.3% | EUR Brandenburg CPI (MoM) (Aug)
+#04:00: 	6.0%	NA	6.1% | EUR Hesse CPI (YoY) (Aug)
+#04:00: 	0.2%	NA	0.3% | EUR Hesse CPI (MoM) (Aug)
+#04:00: 	0.3%	NA	0.2% | EUR North Rhine Westphalia CPI (MoM) (Aug)
+#04:00: 	9.0	8.6	4.9 | EUR Consumer Inflation Expectation (Aug)
+#05:00: 	1.30%	7.50%	-0.50% | EUR Italian Industrial Sales (YoY) (May)
+#05:00: 	0.40%	-0.10%	1.60% | EUR Italian Industrial Sales (MoM) (Jun)
+#05:00: 	6.8%	NA	6.7% | EUR Saxony CPI (YoY) (Aug)
+#05:00: 	0.3%	NA	0.3% | EUR Saxony CPI (MoM) (Aug)
+#05:00: 	-0.33	NA	-0.09 | EUR Business Climate (Aug)
+#05:30: 	4.20%	NA	4.10% | EUR Italian 10-Year BTP Auction
+#05:30: 	3.79%	NA	3.73% | EUR Italian 5-Year BTP Auction
+#07:00: 	7.31%	NA	7.31% | USD MBA 30-Year Mortgage Rate
+#07:00: 	2.3%	NA	-4.2% | USD MBA Mortgage Applications (WoW)
+#07:00: 	144.9	NA	142.0 | USD MBA Purchase Index
+#07:00: 	189.0	NA	184.8 | USD Mortgage Market Index
+#07:00: 	407.1	NA	397.1 | USD Mortgage Refinance Index
+#07:00: 	-6.6	-14.0	-9.4 | EUR Spanish Business Confidence
+#08:00: 	0.3%	0.3%	0.3% | EUR German CPI (MoM) (Aug)
+#08:00: 	6.1%	6.0%	6.2% | EUR German CPI (YoY) (Aug)
+#08:00: 	0.4%	0.3%	0.5% | EUR German HICP (MoM) (Aug)
+#08:00: 	6.4%	6.3%	6.5% | EUR German HICP (YoY) (Aug)
+#08:15: 	177K	195K	371K | USD ADP Nonfarm Employment Change (Aug)
+#08:30: 	3.70%	3.80%	4.90% | USD Core PCE Prices (Q2)
+#08:30: 	1.6%	-5.9%	-5.9% | USD Corporate Profits (QoQ) (Q2)
+#08:30: 	2.1%	2.4%	2.0% | USD GDP (QoQ) (Q2)
+#08:30: 	2.0%	2.2%	4.1% | USD GDP Price Index (QoQ) (Q2)
+#08:30: 	2.2%	2.3%	4.2% | USD GDP Sales (Q2)
+#08:30: 	-91.18B	-89.94B	-88.83B | USD Goods Trade Balance (Jul)
+#08:30: 	2.5%	2.6%	4.1% | USD PCE Prices (Q2)
+#08:30: 	1.7%	1.6%	4.2% | USD Real Consumer Spending (Q2)
+#08:30: 	0.1%	NA	0.2% | USD Retail Inventories Ex Auto (Jul)
+#08:30: 	-0.1%	-0.4%	-0.7% | USD Wholesale Inventories (MoM)
+#10:00: 	0.9%	-0.6%	0.4% | USD Pending Home Sales (MoM) (Jul)
+#10:00: 	77.6	NA	76.9 | USD Pending Home Sales Index (Jul)
+#10:30: 	-10.584M	-3.267M	-6.135M | USD Crude Oil Inventories
+#10:30: 	-0.173M	NA	0.030M | USD EIA Refinery Crude Runs (WoW)
+#10:30: 	-0.586M	-1.555M	0.116M | USD Crude Oil Imports
+#10:30: 	-1.504M	-2.440M	-3.133M | USD Cushing Crude Oil Inventories
+#10:30: 	-0.043M	0.006M	0.337M | USD Distillate Fuel Production
+#10:30: 	1.235M	0.189M	0.945M | USD EIA Weekly Distillates Stocks
+#10:30: 	0.290M	-0.073M	0.130M | USD Gasoline Production
+#10:30: 	0.170M	0.197M	0.502M | USD Heating Oil Stockpiles
+#10:30: 	-1.2%	NA	-0.2% | USD EIA Weekly Refinery Utilization Rates (WoW)
+#10:30: 	-0.214M	-0.933M	1.467M | USD Gasoline Inventories
+#19:50: 	NA	NA	-263.2B | JPY Foreign Bonds Buying
+#19:50: 	NA	NA	-740.7B | JPY Foreign Investments in Japanese Stocks
+#19:50: 	NA	-1.4%	2.4% | JPY Industrial Production (MoM) (Jul)
+#19:50: 	NA	NA	-0.2% | JPY Industrial Production forecast 1m ahead (MoM) (Aug)
+#19:50: 	NA	NA	1.1% | JPY Industrial Production forecast 2m ahead (MoM) (Sep)
+#19:50: 	NA	NA	4.0% | JPY Large Scale Retail Sales YoY (YoY) (Jul)
+#19:50: 	NA	5.4%	5.9% | JPY Retail Sales (YoY) (Jul)
+#21:30: 	NA	NA	51.1 | CNY Chinese Composite PMI (Aug)
+#21:30: 	NA	49.5	49.3 | CNY Manufacturing PMI (Aug)
+#21:30: 	NA	51.1	51.5 | CNY Non-Manufacturing PMI (Aug) 
 # 
 #### M&A Activity: 
-# - M&A also in the headlines again following a busy Friday afternoon, with weekend report FTC pausing its challenge to the ~$28B takeover of HZNP by AMGN to allow for settlement talks.
+# 
+# 
 #### Headlines: 
-# - There is (as has become typical) some focus today on China, specifically the raft of stimulus and reform measures Beijing announced this weekend, though the moves seemed to underwhelm and China's stock market pared gains after opening notably higher.
-#- China increases product export quotas: Platts reports that its sources say China has issued additional oil
-#- product export quotas, though no details were available on the volume of those new quotas. The article quotes analysts arguing Beijing's move may be aimed at supporting domestic industrial activities and could help increase China's crude imports as well.
-#- Shell and Total looking for offshore opportunities near Namibi
-#- Federal Reserve H.8 data from last week showed a drop in securities' balance and higher loan balance compared to the prior week. Commercial real estate loans added $2.3B while consumer loans inched $0.5B higher. Other loans shed $0.3B. Deposits declined $48.8B as large
-#- time deposits rose $13.7B with core deposits declining by $62.5B.
+#
 
+### Script
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
@@ -96,7 +160,6 @@ import datetime as dt
 import math
 import warnings
 warnings.simplefilter("ignore")
-
 # Function
 class yahoo:
     def get_open(link):
@@ -121,43 +184,9 @@ def write_indicator_commentary(row):
     cur = str(row["Cur"])
     event = str(row["Event"])
     actual = str(row["Actual"])
-    if actual == "Missing":
-        actual_float = np.nan
-    else:
-        actual_float = float(row["Actual"].replace("%",""))
-    
     forecast = str(row["Forecast"])
-    if forecast == "Missing":
-        forecast_float = np.nan
-    else:
-        forecast_float = float(row["Forecast"].replace("%",""))
-
     previous = str(row["Previous"])
-    if previous == "Missing":
-        previous_float = np.nan
-    else:
-        previous_float = float(row["Previous"].replace("%",""))
-
-    if actual_float == "Missing":
-        actual_vs_forecast = ""
-    elif actual_float > forecast_float:
-        actual_vs_forecast = "beating consensus of"
-    elif actual_float < forecast_float:
-        actual_vs_forecast = "missing consensus of"
-    else:
-        actual_vs_forecast = "at consensus of"
-        
-    
-    if actual == "Missing":
-        if forecast == "Missing":
-            return f"{time}: {cur} {event} releasing soon, previous figure at {previous}"
-        else:
-            return f"{time}: {cur} {event} releasing soon, forecasted at {forecast} vs. previous {previous}"
-    else:
-        if forecast == "Missing":
-            return f"{time}: {cur} {event} at {actual} vs. previous {previous}" 
-        else:
-            return f"{time}: {cur} {event} at {actual}, {actual_vs_forecast} {forecast} vs. previous {previous}"
+    return f"{time}: \t{actual}\t{forecast}\t{previous} | {cur} {event}"
 
 # Yahoo Finance Data
 yahoo_data = {
@@ -165,7 +194,12 @@ yahoo_data = {
     "DJI":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5EDJI?p=%5EDJI"),
     "NASDAQ":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5EIXIC?p=^IXIC&.tsrc=fin-srch"),
     "S&P/TSX":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5EGSPTSE?p=%5EGSPTSE"),
+    "Russell 2000":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5ERUT?p=%5ERUT"),
+    "Nikkei 225":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5EN225?p=%5EN225"),
+    "FTSE 100":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5EFTSE?p=%5EFTSE"),
+    "Hang Seng Index":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5EHSI?p=^HSI&.tsrc=fin-srch"),
     "VIX":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5EVIX?p=^VIX&.tsrc=fin-srch"),
+    "US Dollar Index":yahoo.get_open("https://ca.finance.yahoo.com/quote/%5ENYICDX?p=^NYICDX&.tsrc=fin-srch"),
     "USD/CAD":yahoo.get_open("https://ca.finance.yahoo.com/quote/CAD=X?p=CAD=X&.tsrc=fin-srch"),
     "EUR/USD":yahoo.get_open("https://ca.finance.yahoo.com/quote/EURUSD=X?p=EURUSD=X&.tsrc=fin-srch"),
     "USD/CNY":yahoo.get_open("https://ca.finance.yahoo.com/quote/CNY=X?p=CNY=X&.tsrc=fin-srch")
@@ -199,6 +233,7 @@ with urlopen(Request(url = "https://www.investing.com/economic-calendar/", heade
 economic_events = pd.read_html(webpage)[2]
 economic_events.columns = ["Time", "Cur", "Importance", "Event", "Actual", "Forecast", "Previous", "Extra1", "Extra2"]
 economic_events = economic_events.drop(columns=["Extra1", "Extra2"], index=0)
+economic_events = economic_events[economic_events["Cur"].isin(["JPY", "EUR", "CNY", "USD", "CAD"])]
 
 if len(list(economic_events[economic_events["Importance"] == "Holiday"]["Event"].drop_duplicates())) == 0:
     holidays = "No market holidays today"
@@ -210,7 +245,7 @@ speeches["Final"] = economic_events["Event"].astype(str) + " at " + economic_eve
 speeches_string = "\n".join(list(speeches["Final"]))
 
 indicators = economic_events[~((economic_events["Event"].isin(speeches["Event"])) | (economic_events["Event"].isin(economic_events[economic_events["Importance"] == "Holiday"]["Event"].drop_duplicates())))].reset_index(drop=True)
-indicators = indicators.fillna("Missing")
+indicators = indicators.fillna("NA")
 indicators["Commentary"] = indicators.apply(write_indicator_commentary, axis=1)
 indicators_string = "\n".join(list(indicators["Commentary"]))
 
@@ -241,7 +276,7 @@ for row in range(commodities_df.shape[0]):
     
 text_current = text_current + " \n"
 
-text_current = text_current + f"### Holidays\n{holidays}\n\n### Speeches\n{speeches_string}\n\n### Economic Releases Today\n{indicators_string}"
+text_current = text_current + f"### Holidays\n{holidays}\n\n### Speeches\n{speeches_string}\n\n### Economic Releases Today\n Time: | Actual | Forecast | Previous | Currency | Event |\n{indicators_string}"
                  
 text_current = text_current + " \n"
 text_current = text_current + " \n"
@@ -256,8 +291,8 @@ headlines = input("Headlines? ")
 cleaned_headlines = "\n".join([f"- {n}" for n in [ele.strip() for ele in headlines.split("-") if ele.strip() != ""]])
 text_current = text_current + (f"### Headlines: \n {cleaned_headlines}")
 
+print("________________________")
 print(text_current)
-
 
 ### Some optional economics data
 
